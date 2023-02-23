49108c2e1fa07604a4e162d62764110d8b6de47d
Martin Clark <webdeveloperplug@gmail.com>
2023-02-22 20:36:53 -0600

    Quick bot framework updates

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
-const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
+const { Telegraf } = require('telegraf');
 
-const token = process.env.TOKEN;
-//const bot = new TelegramBot(dummy, {polling: true});
-const bot = new TelegramBot(token, {polling: true});
-bot.on('message', (message) => {
-    let chat_id = message.from.id;
-    let alias = message.from.first_name;
-    bot.sendMessage(chat_id, 'Hello ' + alias + ', how are you doing today?');
-    //console.log(message);
-});
+const bot = new Telegraf(process.env.TOKEN);
 
-function stopProfaneText() {
-    const profanity = ['bitch', 'shit', 'ho', 'hoe', 'dumb', 'stupid', 'ass', 'fuck', 'asshole', 'gay', 'faggot'];
-    let chat_id = message.from.id;
-    bot.addListener('message', (message) => {
-        if (message === profanity.length) {
-            bot.deleteMessage(chat_id, message);
-            bot.sendMessage(chat_id, 'Please do not use profane language.');
-        }
-    })
-}
-
-bot.onReplyToMessage('message', (message) => {
-    let chat_id = message.from.id;
-    let alias = message.from.username;
-});
+bot.start((ctx) => {
+    ctx.reply('Hello, you have entered our new group!');
+})
+bot.launch();
